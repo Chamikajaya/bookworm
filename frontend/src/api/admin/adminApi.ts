76723cb1ae1d5b92816121151b3fc22bba374296
Chamika Jayasinghe <chamika.21@cse.mrt.ac.lk>
2025-10-06 22:15:46 +0530

    implemented admin api slice for create book, for generating upload url, uploading directly to s3 and update image key mutations

---
+++ frontend/src/api/admin/adminApi.ts
@@ -0,0 +1,133 @@
+import type {
+  CreateBookInput,
+  UploadUrlRequest,
+  UploadUrlResponse,
+} from "@/types/bookTypes";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+export const adminApi = createApi({
+  reducerPath: "adminApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: API_BASE_URL,
+  }),
+  tagTypes: ["Books"],
+  endpoints: (builder) => ({
+    // Create a book
+    createBook: builder.mutation<{ message: string }, CreateBookInput>({
+      query: (book) => ({
+        url: "/books",
+        method: "POST",
+        body: book,
+      }),
+      invalidatesTags: [{ type: "Books", id: "PARTIAL-LIST" }],
+    }),
+    // get the pre-signed url for image upload
+    generateUploadUrl: builder.mutation<
+      UploadUrlResponse,
+      { bookId: string } & UploadUrlRequest
+    >({
+      query: ({ bookId, ...body }) => ({
+        url: `/books/${bookId}/upload-url`,
+        method: "POST",
+        body,
+      }),
+    }),
+    // update book image
+    updateBookImage: builder.mutation<
+      void,
+      { bookId: string; coverImageKey: string }
+    >({
+      query: ({ bookId, coverImageKey }) => ({
+        url: `/books/${bookId}`,
+        method: "PUT",
+        body: { coverImageKey },
+      }),
+      invalidatesTags: (_result, _error, { bookId }) => [
+        { type: "Books", id: bookId },
+      ],
+    }),
+    // upload to s3 via the received pre-signed url - direct s3 bucket upload not via our api
+    uploadToS3: builder.mutation<void, { url: string; file: File }>({
+      queryFn: async ({ url, file }) => {
+        try {
+          const response = await fetch(url, {
+            method: "PUT",
+            body: file,
+            headers: {
+              "Content-Type": file.type,
+            },
+          });
+
+          if (!response.ok) {
+            return {
+              error: {
+                status: response.status,
+                data: { message: "Upload failed" },
+              },
+            };
+          }
+
+          return { data: undefined };
+        } catch (error) {
+          return {
+            error: {
+              status: "FETCH_ERROR",
+              error: String(error),
+            },
+          };
+        }
+      },
+    }),
+
+    // uploadToS3: builder.mutation<
+    //   void,
+    //   { url: string; file: File; onProgress?: (progress: number) => void }
+    // >({
+    //   queryFn: async ({ url, file, onProgress }) => {
+    //     try {
+    //       const xhr = new XMLHttpRequest();
+
+    //       return await new Promise((resolve, reject) => {
+    //         xhr.upload.addEventListener("progress", (e) => {
+    //           if (e.lengthComputable && onProgress) {
+    //             const progress = (e.loaded / e.total) * 100;
+    //             onProgress(progress);
+    //           }
+    //         });
+
+    //         xhr.addEventListener("load", () => {
+    //           if (xhr.status >= 200 && xhr.status < 300) {
+    //             resolve({ data: undefined });
+    //           } else {
+    //             reject({
+    //               error: { status: xhr.status, data: "Upload failed" },
+    //             });
+    //           }
+    //         });
+
+    //         xhr.addEventListener("error", () => {
+    //           reject({
+    //             error: { status: "FETCH_ERROR", data: "Network error" },
+    //           });
+    //         });
+
+    //         xhr.open("PUT", url);
+    //         xhr.setRequestHeader("Content-Type", file.type);
+    //         xhr.send(file);
+    //       });
+    //     } catch (error) {
+    //       return { error: { status: "FETCH_ERROR", data: error } };
+    //     }
+    //   },
+    // }),
+  }),
+});
+
+export const {
+  useCreateBookMutation,
+  useGenerateUploadUrlMutation,
+  useUpdateBookImageMutation,
+  useUploadToS3Mutation,
+} = adminApi;
--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -1,12 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { booksApi } from "./api/booksApi";
+import { adminApi } from "./api/admin/adminApi";
 
 export const store = configureStore({
   reducer: {
     [booksApi.reducerPath]: booksApi.reducer, // auto-generated reducer for bookSlice
+    [adminApi.reducerPath]: adminApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(booksApi.middleware),
+    getDefaultMiddleware()
+      .concat(booksApi.middleware)
+      .concat(adminApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
