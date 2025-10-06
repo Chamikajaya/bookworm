b64540679afb8d46afda7db87c1a46cd30bcc8df
Chamika Jayasinghe <chamika.21@cse.mrt.ac.lk>
2025-10-06 09:53:45 +0530

    created store and used booksApi slice

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,12 +4,16 @@ import { Toaster } from "./components/ui/sonner";
 import "./index.css";
 import App from "./App.tsx";
 import { ThemeProvider } from "./components/ThemeProvider.tsx";
+import { Provider } from "react-redux";
+import { store } from "./store.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <App />
-      <Toaster />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <App />
+        <Toaster />
+      </ThemeProvider>
+    </Provider>
   </StrictMode>
 );
